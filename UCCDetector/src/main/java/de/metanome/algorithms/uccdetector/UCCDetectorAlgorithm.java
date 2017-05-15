0b46a8af021ba0e1dbd3b7e256d61dbb3c93de34
tim-friedrich <tim.friedrich@student.hpi.uni-potsdam.de>
2017-05-15 15:08:34 +0200

    first trivia solution without proning

--- UCCDetector/src/main/java/de/metanome/algorithms/uccdetector/UCCDetectorAlgorithm.java
+++ UCCDetector/src/main/java/de/metanome/algorithms/uccdetector/UCCDetectorAlgorithm.java
@@ -2,8 +2,12 @@ package de.metanome.algorithms.uccdetector;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
 
 import de.metanome.algorithm_integration.AlgorithmConfigurationException;
 import de.metanome.algorithm_integration.AlgorithmExecutionException;
@@ -26,9 +30,10 @@ public class UCCDetectorAlgorithm {
 	
 	protected String relationName;
 	protected List<String> columnNames;
-	
+	protected List<List<String>> records;
 	protected String someStringParameter;
 	protected Integer someIntegerParameter;
+	private Integer depth = 0;
 	protected Boolean someBooleanParameter;
 	
 	public void execute() throws AlgorithmExecutionException {
@@ -39,7 +44,7 @@ public class UCCDetectorAlgorithm {
 		// Example: Initialize
 		this.initialize();
 		// Example: Read input data
-		List<List<String>> records = this.readInput();
+		this.records = this.readInput();
 		// Example: Print what the algorithm read (to test that everything works)
 		this.print(records);
 		// Example: Generate some results (usually, the algorithm should really calculate them on the data)
@@ -89,16 +94,81 @@ public class UCCDetectorAlgorithm {
 	
 	protected List<UniqueColumnCombination> generateResults() {
 		List<UniqueColumnCombination> results = new ArrayList<>();
-		ColumnPermutation lhs = new ColumnPermutation(this.getRandomColumn(), this.getRandomColumn());
-		ColumnPermutation rhs = new ColumnPermutation(this.getRandomColumn(), this.getRandomColumn());
-		UniqueColumnCombination ucc = new UniqueColumnCombination(this.getRandomColumn());
-		results.add(ucc);
+		List<ColumnCombination> columnCombinations = null;
+		while(true){
+			columnCombinations = getNextColumnCombination(columnCombinations);
+			if(columnCombinations == null || columnCombinations.size() == 0){
+				break;
+			}
+			for(ColumnCombination columnComb : columnCombinations){
+				if(checkUniquinessFor(columnComb)){
+					System.out.println("Column is unique");
+					UniqueColumnCombination ucc = new UniqueColumnCombination(columnComb);
+					results.add(ucc);
+				} else{
+					System.out.println("Column is not unique");
+				}
+			}
+			
+		}
 		return results;
+		
 	}
 	
-	protected ColumnIdentifier getRandomColumn() {
-		Random random = new Random(System.currentTimeMillis());
-		return new ColumnIdentifier(this.relationName, this.columnNames.get(random.nextInt(this.columnNames.size())));
+	private boolean checkUniquinessFor(ColumnCombination colComb){
+		Set<ColumnIdentifier> identifiers = colComb.getColumnIdentifiers();
+		Map<String, Integer> resultMap = new HashMap<String, Integer>();
+		for(List<String> record : this.records){
+			String key = "";
+			for(ColumnIdentifier identifier : identifiers){
+				key += record.get(this.columnNames.indexOf(identifier.getColumnIdentifier()));
+			}
+			if(resultMap.containsKey(key)){
+				return false;
+			}
+			resultMap.put(key, 1);
+		}
+		return true;
+	}
+	
+	private List<ColumnCombination> getNextColumnCombination(List<ColumnCombination> previousCombinations){
+		List<ColumnCombination> combinations = new ArrayList<ColumnCombination>();
+		if(previousCombinations == null){
+			for(String columnName : this.columnNames){
+				combinations.add(new ColumnCombination(new ColumnIdentifier(this.relationName, columnName)));
+			}
+		} else{
+			
+		}
+		
+		if(depth==1){
+			for(int i=0; i<this.columnNames.size();i++){
+				for(int j=i+1; j<this.columnNames.size();j++){
+					combinations.add(new ColumnCombination(new ColumnIdentifier(this.relationName, this.columnNames.get(i)), new ColumnIdentifier(this.relationName, this.columnNames.get(j))));
+				}
+			}
+		}
+		if(depth>2){
+			for(int i=0; i<previousCombinations.size(); i++){
+				for(int j=i; j<previousCombinations.size()-1; j++){
+					if(samePrefix(previousCombinations.get(i), previousCombinations.get(j))){
+						Set<ColumnIdentifier> identifiers = previousCombinations.get(i).getColumnIdentifiers();
+						identifiers.addAll(previousCombinations.get(j).getColumnIdentifiers());
+						ColumnCombination comb = new ColumnCombination();
+						comb.setColumnIdentifiers(identifiers);
+						combinations.add(comb);
+					}
+					previousCombinations.get(i);
+				}
+			}
+		}
+		
+		depth++;
+		return combinations;
+	}
+	
+	private boolean samePrefix(ColumnCombination comb1, ColumnCombination comb2){
+		return comb1.compareTo(comb2) == 1;
 	}
 	
 	protected void emit(List<UniqueColumnCombination> results) throws CouldNotReceiveResultException, ColumnNameMismatchException {
